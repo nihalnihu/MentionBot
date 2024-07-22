6f7a0b145d438ae99ec72811921fd7ce11b7320e
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-22 13:49:34 +0530

    Update web.py

--- web.py
+++ web.py
@@ -18,11 +18,11 @@ API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-pyrogram_app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @flask_app.route('/health', methods=['GET'])
 def health_check():
-    bot_status = "running" if pyrogram_app.is_running() else "not running"
+    bot_status = "running" if app.is_running() else "not running"
     return f"Flask and bot status: {bot_status}", 200
 
 def run_flask_app():
@@ -31,13 +31,13 @@ def run_flask_app():
 
 def run_pyrogram_bot():
     try:
-        pyrogram_app.run()
+        app.run()
     except Exception as e:
         flask_app.logger.error(f"An error occurred with Pyrogram: {e}")
 
 def signal_handler(sig, frame):
     print('Signal received, shutting down...')
-    pyrogram_app.stop()  # Gracefully stop the Pyrogram bot
+    app.stop()  # Gracefully stop the Pyrogram bot
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
