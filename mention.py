3fdac5cf788e59051144d61938cf42d017542758
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-22 13:32:41 +0530

    Update mention.py

--- mention.py
+++ mention.py
@@ -3,15 +3,10 @@ import asyncio
 import logging
 import stats
 import os
-from flask import Flask
-import threading
 from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 from stats import check_subscription
-
-
-app = Flask(__name__)
-
+  
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN', '6865008064:AAFROMPKO3aJ7LOP7L6xwnnzw1o6np7KupU')                                                     
@@ -372,20 +367,4 @@ async def users(client, message):
 
 
 
-
-
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    return "OK", 200
-
-def run_flask_app():
-    port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
-
-def run_bot():
-    bot.run()
-
-if __name__ == "__main__":
-    threading.Thread(target=run_flask_app).start()
-    run_bot()
+app.run()
