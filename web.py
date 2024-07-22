c841494c6cea53f0e75bfcdf509fa309826c75a5
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-22 13:06:33 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
-
-@app.route('/health')
-def health_check():
-    return jsonify({"status": "healthy"}), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
