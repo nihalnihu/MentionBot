26155dd465793ed5c887257f127cc94a5f417ac6
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-23 07:41:22 +0530

    Update mention.py

--- mention.py
+++ mention.py
@@ -56,7 +56,7 @@ async def is_user_admin(chat_id, user_id):
 @app.on_message(filters.command("mention") & filters.group)
 async def mention(client, message):
     user_id = message.from_user.id
-    chat_id = message.chat.id
+    chat = message.chat
     mention = message.from_user.mention
     if not already_db(user_id):
         await message.reply_text(
@@ -64,6 +64,16 @@ async def mention(client, message):
         )
         return
     add_group(chat_id)
+
+    try:
+        title = chat.title
+        username = chat.username if chat.username else "None"
+        add_group(chat.id, title, username)
+    except (UserIsBlocked, PeerIdInvalid):
+        pass
+    except Exception as err:
+        print(str(err))
+
     
     logger.info(f"Chat ID: {chat_id}, User ID: {user_id}")
 
@@ -413,9 +423,11 @@ async def callback(client, query):
 @app.on_message(filters.command("stats") & filters.private & filters.user(OWNER_ID))
 async def stats(client, message):
     ALL_USERS = all_users()
-    ALL_GROUPS = all_groups()
-    await message.reply_text(f"Users: {ALL_USERS}\nGroups: {ALL_GROUPS}")
-
+    groups_info = all_groups()
+    
+    groups_info_str = "\n".join(groups_info) if groups_info else "No groups found."
+    
+    await message.reply_text(text=f"Stats for {app.me.mention}\n🙋‍♂️ Users : {ALL_USERS}\n👥 Groups : {len(groups_info)}\n\nAll Groups info:\n{groups_info_str}")
 
 
 @app.on_message(filters.command("group_bc") & filters.private & filters.user(OWNER_ID))
